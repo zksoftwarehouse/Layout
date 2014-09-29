1caf54c54e74671ca3f5adcd5f2841b72244b65e
zanuzzorz <zanuzzorz@gmail.com>
2014-09-29 12:36:38 -0300

    Ajustes nos textos da pagina inicial e sobre nos

--- Portal/zk/raiz/conteudos/meio/conteudo_inicial.php
+++ Portal/zk/raiz/conteudos/meio/conteudo_inicial.php
@@ -7,28 +7,28 @@
 		<!-- -->
     	<div class="col-xs-12 col-sm-12 col-md-11 col-lg-11 div-desc">
 	      <h1 class="titulo-meio">ZK Software House</h1>
-	      <p class="texto-meio">A ZK Software House possui aplicações que fazem parte do conjunto de soluções ZK (Compactação, criptografia e busca em banco de dados) trarão reconhecimento para as referidas fórmulas e com grande potencial de impulsionar diversas áreas.</p>
+	      <p class="texto-meio">A ZK Software House possui aplicações (Compactação, criptografia e busca em banco de dados), que utilizam algoritmos próprios feitos a partir da utilização de um conjunto de fórmulas matemáticas, denominado, Séries Infinitas Com Razões Múltiplas - SRM.</p>
 	      <p class="texto-meio">Os algoritmos ZK são diferenciados por utilizarem uma Matemática Computacional inédita, por estarem sendo preparados para preencherem uma lacuna muito grande no mercado nacional.</p>
     	</div>
     
 
 	   <!-- -->
     	<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 div-desc-compac">
-	    	<img class="img-responsive" src="/zk/raiz/imagens/meio/compacta.png">
+	    	<img class="img-responsive" src="/NovoPortal/zk/raiz/imagens/meio/compacta.png">
 	      <h1 class="titulo-meio">COMPACTADOR</p>
 	      <p class="texto-meio">O Compactador ZK é o único do mundo que não utiliza redundância de dados e que permite criptografar os arquivos compactados, revolucionará os processos de armazenamento de dados e a segurança da Informação</p>
     	</div>
 
 	   <!-- -->
 	   <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 div-desc-cript">
-	    	<img class="img-responsive" src="/zk/raiz/imagens/meio/criptografia.png">
+	    	<img class="img-responsive" src="/NovoPortal/zk/raiz/imagens/meio/criptografia.png">
 	      <h1 class="titulo-meio">CRIPTOGRAFIA</p>
 	      <p class="texto-meio">Matemática Computacional inédita (aprovada por Universidades do Brasil e Canadá, jamais publicadas em livros ou e-books), e inspirada em ISAAC NEWTON</p>
 	    </div>
 
 	   <!-- -->
 	   <div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 div-desc-outros">
-	    	<img class="img-responsive" src="/zk/raiz/imagens/meio/criptografia.png">
+	    	<img class="img-responsive" src="/NovoPortal/zk/raiz/imagens/meio/criptografia.png">
 	      <h1 class="titulo-meio">OUTROS</p>
 	      <p class="texto-meio-outros">Saiba mais sobre nossos produtos.</p>	
 	   </div>
